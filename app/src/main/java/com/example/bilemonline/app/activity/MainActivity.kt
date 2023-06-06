f28782659b8215cf6673b83737927fcc8a55f48d
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2023-06-06 11:37:02 +0300

    Add logo icon

--- app/src/main/java/com/example/bilemonline/app/activity/MainActivity.kt
+++ app/src/main/java/com/example/bilemonline/app/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.bilemonline
+package com.example.bilemonline.app.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bilemonline.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
