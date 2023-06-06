fef83513cdc52edcd52103f9ee203943b559a8f3
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2023-06-06 14:40:57 +0300

    Add splash screen

---
+++ app/src/main/java/com/example/bilemonline/app/activity/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package com.example.bilemonline.app.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        finish()
+    }
+}
