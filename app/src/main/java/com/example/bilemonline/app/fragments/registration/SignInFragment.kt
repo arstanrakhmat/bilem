d594eb09a1eaa9aa84a07b7c8d2ba1505c3cb6e7
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2023-06-11 18:29:13 +0300

    Add login and sign-in screens layout

--- app/src/main/java/com/example/bilemonline/app/fragments/registration/SignInFragment.kt
+++ app/src/main/java/com/example/bilemonline/app/fragments/registration/SignInFragment.kt
@@ -1,7 +1,11 @@
 package com.example.bilemonline.app.fragments.registration
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.bilemonline.R
 import com.example.bilemonline.app.fragments.BaseFragment
 import com.example.bilemonline.databinding.FragmentSignInBinding
 
@@ -12,6 +16,14 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>() {
         container: ViewGroup?
     ): FragmentSignInBinding {
         return FragmentSignInBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_registrationFragment)
+        }
     }
 
 }
