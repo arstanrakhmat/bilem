820734f67ae6138e5bd4ed313371aa3f24d1e444
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2023-08-17 19:32:04 +0300

    Add logic for text and video fragment

--- app/src/main/java/com/example/bilemonline/app/fragments/section/sectionType/TextFragment.kt
+++ app/src/main/java/com/example/bilemonline/app/fragments/section/sectionType/TextFragment.kt
@@ -1,17 +1,25 @@
 package com.example.bilemonline.app.fragments.section.sectionType
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.example.bilemonline.app.fragments.BaseFragment
 import com.example.bilemonline.data.model.ContentX
 import com.example.bilemonline.databinding.FragmentTextBinding
 
-class TextFragment(textContent: ContentX) : BaseFragment<FragmentTextBinding>() {
+class TextFragment(private val textContent: ContentX) : BaseFragment<FragmentTextBinding>() {
     override fun inflateView(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentTextBinding {
         return FragmentTextBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvHeader.text = textContent.id
+        binding.tvContent.text = textContent.content
     }
 
 }
--- app/src/main/java/com/example/bilemonline/app/fragments/section/sectionType/VideoFragment.kt
+++ app/src/main/java/com/example/bilemonline/app/fragments/section/sectionType/VideoFragment.kt
@@ -1,12 +1,29 @@
 package com.example.bilemonline.app.fragments.section.sectionType
 
+import android.net.Uri
+import android.os.Bundle
+import com.google.android.exoplayer2.MediaItem
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.example.bilemonline.app.fragments.BaseFragment
 import com.example.bilemonline.data.model.ContentX
 import com.example.bilemonline.databinding.FragmentVideoBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.SimpleExoPlayer
 
-class VideoFragment(videoContent: ContentX) : BaseFragment<FragmentVideoBinding>() {
+class VideoFragment(private val videoContent: ContentX) : BaseFragment<FragmentVideoBinding>() {
+
+    private var exoPlayer: ExoPlayer? = null
+    private var simpleExoPlayer: SimpleExoPlayer? = null
+
+    companion object {
+        const val URL = "https://kinescope.io/embed/204770908"
+        const val URL2 = "https://kinescope.io/204770908"
+        const val videoUrl =
+            "https://media.geeksforgeeks.org/wp-content/uploads/20201217192146/Screenrecorder-2020-12-17-19-17-36-828.mp4?_=1"
+        const val URL3 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    }
 
     override fun inflateView(
         inflater: LayoutInflater,
@@ -14,4 +31,32 @@ class VideoFragment(videoContent: ContentX) : BaseFragment<FragmentVideoBinding>
     ): FragmentVideoBinding {
         return FragmentVideoBinding.inflate(inflater, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvHeader.text = videoContent.header
+        preparePlayer4()
+    }
+
+    private fun preparePlayer2() {
+        exoPlayer = ExoPlayer.Builder(requireContext()).build()
+        binding.playerView.player = exoPlayer
+        val uri = Uri.parse(URL)
+        val mediaItem = MediaItem.fromUri(uri)
+        exoPlayer?.addMediaItem(mediaItem)
+        exoPlayer?.seekTo(0)
+        exoPlayer?.playWhenReady = false
+        exoPlayer?.prepare()
+
+    }
+
+    private fun preparePlayer4() {
+        simpleExoPlayer = SimpleExoPlayer.Builder(requireContext()).build()
+        binding.playerView.player = simpleExoPlayer
+        val mediaItem = MediaItem.fromUri(URL3)
+        simpleExoPlayer?.addMediaItem(mediaItem)
+        simpleExoPlayer?.prepare()
+        simpleExoPlayer?.playWhenReady = true
+    }
+
 }
